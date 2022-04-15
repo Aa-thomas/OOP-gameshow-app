7dd28d0695d41ac1d44217ac4315c31da9417d7d
Aa-thomas <Aartho95@gmail.com>
2022-04-14 23:27:06 -0400

    complete addPhraseToDisplay function

--- js/Phrase.js
+++ js/Phrase.js
@@ -5,9 +5,20 @@ class Phrase {
     constructor(phrase) {
         this.phrase = phrase.toLowerCase();
     }
-
+    /**
+    * Display phrase on game board
+    */
     addPhraseToDisplay() {
-
+        const phraseToDisplay = this.phrase.split('');
+        
+        
+        phraseToDisplay.forEach( letter => {
+            let letterPlaceHolder = `<li class="hide letter ${letter}">${letter}</li>`;
+            if (letter === ' ') {
+                letterPlaceHolder = `<li class="space"> </li>`;   
+            }
+            document.querySelector('ul').insertAdjacentHTML('beforeend', letterPlaceHolder);
+        });
     }
 
     checkLetter() {
